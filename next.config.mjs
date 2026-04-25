3c748b4ba4df906968dee2f7a998c11955d60853
APPLE <apple@APPLEs-MacBook-Pro.local>
2026-04-25 10:36:07 +0300

    updating next-config

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['localhost', 'images.unsplash.com'],
+        domains: [
+            'localhost',
+            'images.unsplash.com',
+            'public.blob.vercel-storage.com',
+        ],
     },
 };
 
